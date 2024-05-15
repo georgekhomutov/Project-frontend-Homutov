374757b17b9d3f4ddbb5f980ba8e2f27c9e0a799
George Khomutov <goshaman19@mail.ru>
2024-05-16 04:07:18 +0700

    Updates

---
+++ precache-manifest.00fe5d5c11bcbd2310049be94c4d9fcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5886dc74fb50133d792fa6d5610755b9",
+    "url": "/georgekhomutov/Project-frontend-Homutov/index.html"
+  },
+  {
+    "revision": "505c28a8d505540c4081",
+    "url": "/georgekhomutov/Project-frontend-Homutov/static/css/main.427275ce.chunk.css"
+  },
+  {
+    "revision": "7d6b9f8978281162c50d",
+    "url": "/georgekhomutov/Project-frontend-Homutov/static/js/2.fd5371a8.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/georgekhomutov/Project-frontend-Homutov/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "505c28a8d505540c4081",
+    "url": "/georgekhomutov/Project-frontend-Homutov/static/js/main.26948cee.chunk.js"
+  },
+  {
+    "revision": "4b347a159787ab9dfc7a",
+    "url": "/georgekhomutov/Project-frontend-Homutov/static/js/runtime~main.d02c8408.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/georgekhomutov/Project-frontend-Homutov/static/media/logo.06e73328.svg"
+  }
+]);
